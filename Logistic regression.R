69a006bc8a9cd390bf7588a0e4be7bf2f26140b9
Ronggui <ronggui.huang@gmail.com>
2014-03-12 10:11:04 +0800

    Create Logistic regression.R

---
+++ Logistic regression.R
@@ -0,0 +1,24 @@
+## demonstrating how the parameters change the logistic curve
+curve(plogis(x-8),-20,20,lty=1)
+curve(plogis(x-4),-20,20,lty=2,add=T)
+curve(plogis(x),-20,20,lty=3,add=T)
+curve(plogis(x+4),-20,20,lty=4,add=T)
+curve(plogis(x+8),-20,20,lty=5,add=T)
+
+curve(plogis(x*0.25),-20,20,lty=1)
+curve(plogis(x*0.5),-20,20,lty=2,add=T)
+curve(plogis(x*0.1),-20,20,lty=3,add=T)
+curve(plogis(x*2),-20,20,lty=4,add=T)
+curve(plogis(x*4),-20,20,lty=5,add=T)
+
+curve(plogis(x*-0.25),-20,20,lty=1,col="red",add=T)
+curve(plogis(x*-0.5),-20,20,lty=2,col="red",add=T)
+curve(plogis(x*-0.1),-20,20,lty=3,col="red",add=T)
+curve(plogis(x*-2),-20,20,lty=4,col="red",add=T)
+curve(plogis(x*-4),-20,20,lty=5,col="red",add=T)
+
+## fit a logistic model
+library(car)
+summary(m1 <- glm(lfp~k5+k618+age+wc+hc+lwg+inc,data=Mroz,family=binomial))
+library(coefplot)
+coefplot(m1)
